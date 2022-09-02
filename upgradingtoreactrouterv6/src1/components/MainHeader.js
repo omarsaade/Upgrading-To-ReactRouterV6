912d3e548cd65b119a03a895aa82e83177f30538
omar saade <omarsaadeh11@gmail.com>
2022-09-02 18:47:28 +0300

    react router v6

--- upgradingtoreactrouterv6/src1/components/MainHeader.js
+++ upgradingtoreactrouterv6/src1/components/MainHeader.js
@@ -8,18 +8,19 @@ const MainHeader = () => {
       <nav>
         <ul>
           <li>
-            <NavLink activeClassName={classes.active} to='/welcome'>
+
+            <NavLink className={(navData) => navData.isActive ? classes.active : ''} to='/welcome'>
               Welcome
             </NavLink>
           </li>
           <li>
-            <NavLink activeClassName={classes.active} to='/products'>
+            <NavLink className={(navData) => navData.isActive ? classes.active : ''} to='/products'>
               Products
             </NavLink>
           </li>
         </ul>
-      </nav>
-    </header>
+      </nav >
+    </header >
   );
 };
 
